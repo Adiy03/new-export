355578a03417e06263db2edfdd34c987c6b3afca
Adiy03 <adiyogo3@gmail.com>
2024-05-22 18:24:47 +0700

    gtag2

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,12 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Whatsapp from "@/components/whatsapp/Whatsapp";
 import Head from 'next/head';
+import GoogleAnalytics from "./GoogleAnalytics";
 
 const karla = Karla({
   weight: '400',
   subsets: ['latin'],
-});
+})
 
 export const metadata = {
   title: "EasyExportID | Indonesia Wholesale Trade",
@@ -21,21 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="hCEO0-UprO6Pt_XkGN93_xSz4QMzGyu_wzCzfBlUZbQ" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-72TG333Z14"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-72TG333Z14');
-            `,
-          }}
-        />
-      </Head>
       <body className={karla.className}>
+      <GoogleAnalytics />
         <Navbar />
         {children}
         <Whatsapp />
