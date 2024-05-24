094d5c3a1b8525cd3349a4a3f704a3e146e12151
Adiy03 <adiyogo3@gmail.com>
2024-05-24 20:42:05 +0700

    broken link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,31 +7,31 @@ import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 
 const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 3,
-    title: "Product",
-    url: "/product",
-  },
-  {
-    id: 4,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 5,
-    title: "Inquiry",
-    url: "/inquiry",
-  },
+  // {
+  //   id: 1,
+  //   title: "Home",
+  //   url: "/",
+  // },
+  // {
+  //   id: 2,
+  //   title: "About",
+  //   url: "/about",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Product",
+  //   url: "/product",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Blog",
+  //   url: "/blog",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Inquiry",
+  //   url: "/inquiry",
+  // },
 ];
 
 const Navbar = () => {
